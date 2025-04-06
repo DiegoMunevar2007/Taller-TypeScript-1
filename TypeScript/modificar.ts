ab2f7b102bde37af08d9331686ff4401cdd2d466
Diego Alejandro Munévar Pérez <d.munevar@uniandes.edu.co>
2025-04-05 21:31:04 -0500

    Taller Typescript

--- TypeScript/modificar.ts
+++ TypeScript/modificar.ts
@@ -1,7 +1,11 @@
 import { series } from "./data.js";
 import { Serie } from "./serie.js";
 
+let sumaTemporadas:number=0;
+
 actualizarTabla();
+actualizarSeasonAverage();
+
 function actualizarTabla(){
     var contenidoTabla = document.getElementById("content");
     let nuevoContenido: string = "";
@@ -9,15 +13,24 @@ function actualizarTabla(){
         let serie:Serie = series[i];
         nuevoContenido=nuevoContenido.concat("<tr>"+
             "<td><b>"+serie.numero+"</td>"+
-            "<td><a>"+serie.titulo+"</a></td>"+
+            `<td><a href=\"${serie.info}\" target="_blank">`+serie.titulo+"</a></td>"+
             "<td>"+serie.canal+"</td>"+
             "<td>"+serie.temporadas+"</td>"+
             "</tr>"
             )
+        sumaTemporadas+=serie.temporadas
     }
     if (contenidoTabla){
         contenidoTabla.innerHTML=nuevoContenido;
     } else{
         console.log("no existe")
+    }
+}
+
+function actualizarSeasonAverage(){
+    var contenidoSeason = document.getElementById("season-average");
+    let promedio = sumaTemporadas/series.length;
+    if (contenidoSeason) {
+        contenidoSeason.innerHTML = contenidoSeason.innerHTML + String(promedio);
     }
 }
